89e6567bd6399a2ee78e5923d8e32d50251b927a
FuryAryan <yoyo26new@gmail.com>
2020-04-28 18:45:39 +0530

    Added election project to this repo

---
+++ election/contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Election {
+    // Constructor
+    //Read Candidate name
+    string public candidate;
+    // Constructor
+    function Elections () public {
+        candidate = "Candidate1";
+    }
+
+}
